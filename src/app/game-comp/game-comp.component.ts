cd03427a19c788968ac7814ef28f482a140c7867
AndreiPivoda <andrei.pivoda@gmail.com>
2017-08-06 07:46:59 +0300

    canvas ready

--- src/app/game-comp/game-comp.component.ts
+++ src/app/game-comp/game-comp.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-game-comp',
   templateUrl: './game-comp.component.html',
   styleUrls: ['./game-comp.component.css']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements AfterViewInit {
+
+  context: CanvasRenderingContext2D;
+  @ViewChild('myCanvas') myCanvas;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const canvas = this.myCanvas.nativeElement;
+    this.context = canvas.getContext('2d');
+    this.draw();
   }
 
+  draw() {
+    const ctx = this.context;
+    ctx.clearRect(0, 0, 400, 400);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, 200, 200);
+
+  }
 }
