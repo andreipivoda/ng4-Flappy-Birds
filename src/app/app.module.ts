45880c4440da140c46ed0a0de00d1eea6b5c3c2d
AndreiPivoda <andrei.pivoda@gmail.com>
2017-08-06 07:35:12 +0300

    comps + service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { GameDataService } from './game-data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game-comp/game-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [GameDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/game-comp/game-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-comp',
+  templateUrl: './game-comp.component.html',
+  styleUrls: ['./game-comp.component.css']
+})
+export class GameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
